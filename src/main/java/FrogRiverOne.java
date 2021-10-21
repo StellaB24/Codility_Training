8f4bc0983f30954f2cdb6dcb9197f9f386158c01
Stella <stellab247@gmail.com>
2021-10-21 19:19:25 +0300

    Lesson 3 - unfinished Lesson 4 - FrogRiverOne

--- src/main/java/FrogRiverOne.java
+++ src/main/java/FrogRiverOne.java
@@ -4,7 +4,7 @@ public class FrogRiverOne {
     public int solution(int X, int[] A) {
 
         HashSet<Integer> hashSet = new HashSet<>();
-        for (int i = 1; i < X; i++) {
+        for (int i = 1; i <= X; i++) {
             hashSet.add(i);
         }
 
